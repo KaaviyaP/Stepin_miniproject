394adb009fa419bd2e04cb78ce486725d3675e5a
KaaviyaP <kaaviya2881999@gmail.com>
2021-04-15 18:06:33 +0530

    multi

---
+++ 3_Implementation/src/capacitor.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include "timer_calculator.h"
+
+int capacitor(int code){
+    
+    int cvalue=0;
+    if(code>=999 || code<=0){
+        printf("INVALID INPUT");
+    }
+    else if(code>0 && code<=99){
+        cvalue=code;
+    }
+    else{
+        int mul=code%10;
+        int dig=code/10;
+        cvalue=dig*pow( 10, mul );
+    }
+    printf("Capacitance value: %d", cvalue);
+    printf("pF\n");
+    return cvalue;
+}
---
+++ 3_Implementation/src/resistor.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include "timer_calculator.h"
+
+void    colorcodes( void );     
+int  decode( char code );
+
+
+int resistor(int band,char c1,char c2,char c3){
+   
+       
+    int  resistance=0;
+    int  color1, color2, color3;  
+       
+        
+        color1 = decode( c1 );
+        color2 = decode( c2 );
+        color3 = decode( c3 );
+     
+        if ( color1 == -999  ||  color2 == -999  ||
+          color3 == -999 ){
+          printf( "\n\n\tBad code -- cannot compute resistance\n" );}
+     
+        else {
+          resistance = ( 10 * color1  +  color2 )* pow( 10, color3 );
+          printf( "\n\nResistance value :%d", resistance );
+          printf("ohms\n");
+        }
+        return resistance;
+        
+}
+
+    
+void  colorcodes( void )
+{
+   
+   printf( "Color\t\t\tCode\n\t" );
+   printf( "-----\t\t\t----\n\n" );
+   printf( "\tBlack------> B\n" );
+   printf( "\tBrown------> N\n" );
+   printf( "\tRed--------> R\n" );
+   printf( "\tOrange-----> O\n" );
+   printf( "\tYellow-----> Y\n" );
+   printf( "\tGreen------> G\n" );
+   printf( "\tBlue-------> E\n" );
+   printf( "\tViolet-----> V\n" );
+   printf( "\tGray-------> A\n" );
+   printf( "\tWhite------> W\n" );
+}
+
+
+int decode( char code )
+{
+     switch ( code ) {
+     case 'B':
+          return 0;
+     case 'N':
+          return 1;
+     case 'R':
+          return 2;
+     case 'O':
+          return 3;
+     case 'Y':
+          return 4;
+     case 'G':
+          return 5;
+     case 'E':
+          return 6;
+     case 'V':
+          return 7;
+     case 'A':
+          return 8;
+     case 'W':
+          return 9;
+     default:
+          return -999;     
+     }
+}
+
